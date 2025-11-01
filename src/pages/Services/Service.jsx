16ea1e04edfefeed86121245f5aa89d922985842
Akash Jadhav <akash.j2991@gmail.com>
2025-11-02 01:30:25 +0530

    Designed product section.

--- src/pages/Services/Service.jsx
+++ src/pages/Services/Service.jsx
@@ -1,12 +1,13 @@
 import ServiceBanner from './Sections/ServiceBanner';
 import CoreExpertiseSection from './Sections/CoreExpertise';
 import GrowBeyondSection from '../../CommonSections/GrowBeyond';
-
+import ScrollableProductCardSection from './Sections/ScrollableProductCard'
 export default function Service() {
     return (
         <>
             <ServiceBanner />
             <CoreExpertiseSection />
+            <ScrollableProductCardSection />
             <GrowBeyondSection />
         </>
     );
